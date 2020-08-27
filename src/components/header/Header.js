7bb727eaff3118873e1c1b285e51a37930dc85fb
brycexzh <bryce.xu@hotmail.com>
2020-08-27 21:34:29 +1000

    added details-component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 
 import './Header.scss';
-import logo from '../../assets/cinema-logo.svg';
+import logo from '../../assets/logo.png';
 import {
   getMovies,
   setMovieType,
