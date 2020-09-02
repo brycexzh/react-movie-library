84605bf25d44e21f10ba57232f97990644a3789a
brycexzh <bryce.xu@hotmail.com>
2020-09-03 01:34:01 +1000

    setup sentry for error monitoring

--- src/redux/actions/movies.js
+++ src/redux/actions/movies.js
@@ -1,15 +1,37 @@
-import * as types from '../types';
-import * as movieServices from '../../services/movies.service';
+import {
+  MOVIE_LIST,
+  SET_ERROR,
+  RESPONSE_PAGE,
+  LOAD_MORE_RESULTS,
+  MOVIE_TYPE,
+  SEARCH_QUERY,
+  SEARCH_RESULT,
+  MOVIE_DETAILS,
+  CLEAR_MOVIE_DETAILS
+} from '../types';
+import {
+  MOVIE_API_URL,
+  SEARCH_API_URL,
+  MOVIE_DETAILS_URL,
+  MOVIE_CREDITS_URL,
+  MOVIE_IMAGES_URL,
+  MOVIE_VIDEOS_URL,
+  MOVIE_REVIEWS_URL
+} from '../../services/movies.service';
 
 export const getMovies = (type, pageNumber) => async (dispatch) => {
   try {
     const response = await getMoviesRequest(type, pageNumber);
     const { results, payload } = response;
-    dispatchMethod(types.MOVIE_LIST, results, dispatch);
-    dispatchMethod(types.RESPONSE_PAGE, payload, dispatch);
+    dispatchMethod(MOVIE_LIST, results, dispatch);
+    dispatchMethod(RESPONSE_PAGE, payload, dispatch);
   } catch (error) {
     if (error.response) {
-      dispatchMethod(types.SET_ERROR, error.response.data.message, dispatch);
+      const payload = {
+        message: error.response.data.message || error.response.data.status_message,
+        statusCode: error.response.status
+      };
+      dispatchMethod(SET_ERROR, payload, dispatch);
     }
   }
 };
@@ -19,13 +41,17 @@ export const loadMoreMovies = (type, pageNumber) => async (dispatch) => {
     const response = await getMoviesRequest(type, pageNumber);
     const { results, payload } = response;
     dispatchMethod(
-      types.LOAD_MORE_RESULTS,
+      LOAD_MORE_RESULTS,
       { list: results, page: payload.page, totalPages: payload.totalPages },
       dispatch
     );
   } catch (error) {
     if (error.response) {
-      dispatchMethod(types.SET_ERROR, error.response.data.message, dispatch);
+      const payload = {
+        message: error.response.data.message || error.response.data.status_message,
+        statusCode: error.response.status
+      };
+      dispatchMethod(SET_ERROR, payload, dispatch);
     }
   }
 };
@@ -33,53 +59,57 @@ export const loadMoreMovies = (type, pageNumber) => async (dispatch) => {
 export const searchResult = (query) => async (dispatch) => {
   try {
     if (query) {
-      const movies = await movieServices.SEARCH_API_URL(query);
+      const movies = await SEARCH_API_URL(query);
       const { results } = movies.data;
-      dispatchMethod(types.SEARCH_RESULT, results, dispatch);
+      dispatchMethod(SEARCH_RESULT, results, dispatch);
     } else {
-      dispatchMethod(types.SEARCH_RESULT, [], dispatch);
+      dispatchMethod(SEARCH_RESULT, [], dispatch);
     }
   } catch (error) {
     if (error.response) {
-      dispatchMethod(types.SET_ERROR, error.response.data.message, dispatch);
+      const payload = {
+        message: error.response.data.message || error.response.data.status_message,
+        statusCode: error.response.status
+      };
+      dispatchMethod(SET_ERROR, payload, dispatch);
     }
   }
 };
 
 export const movieDetails = (id) => async (dispatch) => {
   try {
-    const details = await movieServices.MOVIE_DETAILS_URL(id);
-    const credits = await movieServices.MOVIE_CREDITS_URL(id);
-    const images = await movieServices.MOVIE_IMAGES_URL(id);
-    const videos = await movieServices.MOVIE_VIDEOS_URL(id);
-    const reviews = await movieServices.MOVIE_REVIEWS_URL(id);
+    const details = await MOVIE_DETAILS_URL(id);
+    const credits = await MOVIE_CREDITS_URL(id);
+    const images = await MOVIE_IMAGES_URL(id);
+    const videos = await MOVIE_VIDEOS_URL(id);
+    const reviews = await MOVIE_REVIEWS_URL(id);
 
     const resp = await Promise.all([details, credits, images, videos, reviews])
       .then((values) => Promise.all(values.map((value) => value.data)))
       .then((response) => response);
-    dispatchMethod(types.MOVIE_DETAILS, resp, dispatch);
+    dispatchMethod(MOVIE_DETAILS, resp, dispatch);
   } catch (error) {
     if (error.response) {
-      dispatchMethod(types.SET_ERROR, error.response.data.message, dispatch);
+      dispatchMethod(SET_ERROR, error.response.data.message, dispatch);
     }
   }
 };
 
 export const clearMovieDetails = () => async (dispatch) => {
-  dispatchMethod(types.CLEAR_MOVIE_DETAILS, [], dispatch);
+  dispatchMethod(CLEAR_MOVIE_DETAILS, [], dispatch);
 };
 
 export const setResponsePageNumber = (page, totalPages) => async (dispatch) => {
   const payload = { page, totalPages };
-  dispatchMethod(types.RESPONSE_PAGE, payload, dispatch);
+  dispatchMethod(RESPONSE_PAGE, payload, dispatch);
 };
 
 export const setMovieType = (type) => async (dispatch) => {
-  dispatchMethod(types.MOVIE_TYPE, type, dispatch);
+  dispatchMethod(MOVIE_TYPE, type, dispatch);
 };
 
 export const searchQuery = (query) => async (dispatch) => {
-  dispatchMethod(types.SEARCH_QUERY, query, dispatch);
+  dispatchMethod(SEARCH_QUERY, query, dispatch);
 };
 
 const dispatchMethod = (type, payload, dispatch) => {
@@ -87,7 +117,7 @@ const dispatchMethod = (type, payload, dispatch) => {
 };
 
 const getMoviesRequest = async (type, pageNumber) => {
-  const movies = await movieServices.MOVIE_API_URL(type, pageNumber);
+  const movies = await MOVIE_API_URL(type, pageNumber);
   const { results, page, total_pages } = movies.data;
   const payload = {
     page,
